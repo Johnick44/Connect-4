ed9f9deaf8a2308e6da11479d1ae645e6d36a04f
Johnick44 <johnikogreen@gmail.com>
2019-11-01 11:45:21 -0400

    another update

--- Game.py
+++ Game.py
@@ -1,18 +1,16 @@
 
 
 class Game:
-    '''
-     Skeleton of the game connect 4, where Player 1 and Player 2 have to
-    think of a strategy against each other in order to win, first person to put
-    4 signs in a row will win the game, this row can be vertical, horizontal,
-    and diagonal.
-    '''
+
+    # Skeleton of the game connect 4, where Player 1 and Player 2 have to
+    # think of a strategy against each other in order to win, first person
+    # to put 4 signs in a row will win the game, this row can be vertical,
+    # horizontal, and diagonal.
 
     def __init__(self):
-        '''
-        Initializes all of the players movements and also creates the map for
-        the player.
-         '''
+
+        # Initializes all of the players movements and also creates the map for
+        # the player.
         self.moves = 1
         self.current_map = [
             [0, 0, 0, 0, 0, 0, 0],
@@ -22,43 +20,41 @@ class Game:
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0]]
 
-        Game.display_board(self, self.current_map, Bool=False)
+        Game.display(self, self.current_map)
 
-    def display_board(self, maps, Bool: bool):
-        if Bool == False:
+    def display(self, maps):
             print(maps)
-        print("Winner Winner Chicken dinner!")
 
-    def move(self, point: int):
-        '''
-        Makes a player make a command that will allow to put a sign in which
-        the position given'''
-        player1 = X
-        player2 = O
-        if (0 <= point <= 6):
+    def move(self):
+
+        # Makes a player make a command that will allow to put a sign in which
+        # the position given
+        point = int(input("please put a number between 0-6:"))
+        player1 = "X"
+        player2 = "Y"
+        if 0 <= point <= 6:
             if self.moves % 2 == 0:
                 Game.update_map(self, point, player2)
-                Game.display_board(self, self.current_map)
+                Game.display(self, self.current_map)
             else:
                 Game.update_map(self, point, player1)
-                Game.display_board(self, self.current_map)
+                Game.display(self, self.current_map)
+            self.moves += 1
         else:
             raise TypeError
-        self.moves += 1
 
     def update_map(self, position, sign):
-        '''Updates the map so that self.current_map has a new sign.'''
+        # Updates the map so that self.current_map has a new sign.
         count = 0
         while count <= len(self.current_map):
-            if (self.current_map[5 - count][position] == X) \
-                    or (self.current_map[5 - count][position] == O):
+            if (self.current_map[5 - count][position] == "X") \
+                    or (self.current_map[5 - count][position] == "Y"):
                 count += 1
             else:
                 self.current_map[5 - count][position] = sign
                 Game.win(self, sign)
 
     def win(self, sign):
-        winner = False
         column_count = 6
         row_count = 5
         # check for horizontal win
@@ -68,9 +64,8 @@ class Game:
                         and self.current_map[row][column + 1] == sign \
                         and self.current_map[row][column + 2] == sign \
                         and self.current_map[row][column + 3] == sign:
-                    winner = True
-                    Game.display_board(self, self.current_map, winner)
-                    Game.game_over(self)
+                    Game.display(self, self.current_map)
+                    Game.Game_Over(self)
         # check for vertical win
         for column in range(column_count):
             for row in range(row_count - 3):
@@ -78,16 +73,15 @@ class Game:
                         and self.current_map[row + 1][column] == sign \
                         and self.current_map[row + 2][column] == sign \
                         and self.current_map[row + 3][column] == sign:
-                    winner = True
-                    Game.display_board(self, self.current_map, winner)
-                    Game.game_over(self)
+                    Game.display(self, self.current_map)
+                    Game.Game_Over(self)
 
-    def game_over(self):
+    def Game_Over(self):
+        print("Winner Winner Chicken Dinner")
         return True
 
 
 if __name__ == '__main__':
     g = Game()
-    print("I hope this works")
-    while not g.game_over():
-        action = g.move(2)
+    while g.Game_Over():
+        g.move()
